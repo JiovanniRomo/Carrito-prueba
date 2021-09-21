ea18d78a15f7c50d2bf85048f4815563a7d0e621
Jiovanni Romo <srr190802@gmail.com>
2021-09-20 22:56:36 -0500

    Interfaz de index.html finalizada

---
+++ src/models/Curso.ts
@@ -0,0 +1,14 @@
+class Curso {
+    img: string;
+    titulo: string;
+    precio: number;
+    id: number; 
+
+
+    constructor(img: string, titulo: string, precio: number, id: number) {
+        this.img = img;
+        this.titulo = titulo;
+        this.precio = precio;
+        this.id = id;
+    }
+}
